a0e1a002b54679df745f8697a762f79019aa6050
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-01-05 13:41:16 -0800

    writing questions for user input in App. next, execute testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 public class App {
 
     public static void main(String[] args) {
+        boolean programRunning = true;
         //public class Party {
 
             Party guest = new Party();
@@ -24,14 +25,51 @@ public class App {
 //                    PartyCost.add(drink)
 //                    PartyCost.add(entertainment);
 
-            while(true) {
-                System.out.println("How many?");
+            while(programRunning) {
+
+                System.out.println("How many guests will be attending?");
 
                 try{
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
                     String inputString = bufferedReader.readLine();
 
-                    System.out.println("Color entered : " + inputString);
+                    System.out.println("Ok, great, " + inputString + " guests. How many meals will be ordered?");
+
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+                try{
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                    String inputMealString = bufferedReader.readLine();
+
+                    System.out.println("Excellent! " + inputMealString + " meals. How many drinks will be ordered?");
+
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+                try{
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                    String inputDrinkString = bufferedReader.readLine();
+
+                    System.out.println("Also,  " + inputDrinkString + " Drinks.");
+
+
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+                try{
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                    String price = bufferedReader.readLine();
+
+                    System.out.println("Wonderful, your final cost is,  ");
+                    programRunning = false;
+
                 }
                 catch(IOException e)
                 {
