8c941a45f9d36aceca9bec68a4130b42d7764ea8
Danyel Murray <danyelmurray1987@gmail.com>
2018-01-10 16:23:29 -0800

    fix app redundancy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,12 +10,12 @@ public class App {
 
     public static void main(String[] args) {
         boolean programRunning = true;
-<<<<<<< HEAD
-        while(programRunning) {
+
+        while (programRunning) {
 
             System.out.println("How many guests will be attending?");
 
-            try{
+            try {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
                 String guestTotal = bufferedReader.readLine();
 
@@ -28,7 +28,7 @@ public class App {
                 String inputEntertainment = bufferedReader.readLine();
                 boolean yesNo = false;
 
-                if (inputEntertainment.equals("yes")){
+                if (inputEntertainment.equals("yes")) {
                     yesNo = true;
                 }
 
@@ -41,45 +41,12 @@ public class App {
 
                 System.out.println("Wonderful, your final cost is,  $" + newParty.calculateFullPrice());
                 programRunning = false;
-=======
-            while(programRunning) {
 
-                System.out.println("How many guests will be attending?");
 
-                try{
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-                    String guestTotal = bufferedReader.readLine();
-
-                    System.out.println("Ok, great, " + guestTotal + " guests. How many meals will be ordered?");
-                    String inputMealString = bufferedReader.readLine();
-                    System.out.println("Excellent! " + inputMealString + " meals. How many drinks will be ordered?");
-                    String inputDrinkString = bufferedReader.readLine();
-                    System.out.println("Also,  " + inputDrinkString + " Drinks. Do you want to include a DJ for $300? ('yes' or 'no'");
-
-                    String inputEntertainment = bufferedReader.readLine();
-                    boolean yesNo = false;
-
-                    if (inputEntertainment.equals("yes")){
-                        yesNo = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-
-                    System.out.println("Enter your coupon code ('50for100' or '$50off' or enter 'finish' if no coupon.");
-                    String inputCoupon = bufferedReader.readLine();
-
-                    System.out.println(inputCoupon);
-
-                    Party newParty = new Party(Integer.parseInt(guestTotal), Integer.parseInt(inputMealString), Integer.parseInt(inputDrinkString), yesNo, inputCoupon);
-
-                    System.out.println("Wonderful, your final cost is,  " + newParty.calculateFullPrice());
-                    programRunning = false;
->>>>>>> a0e26e25fdb9f768674714db48dee76b0e23404d
-
+                }
             }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
+
         }
-    }
-
-}
--- src/test/java/PartyTest.java
+++ src/test/java/PartyTest.java
@@ -6,12 +6,12 @@ public class PartyTest {
 
 
     @Test
-<<<<<<< HEAD
+
     public void newPartyObjectCorrectlyCreated_true() throws Exception {
         Party party = new Party(12, 12, 12, true, "no");
         assertEquals(true, party instanceof Party);
     }
-=======
+
     public void howManyGuests_enterNumber_getNumber() throws Exception {
         Party testParty = new Party(10,10,10, true, "finish");
         assertEquals(true, testParty instanceof Party);
@@ -19,5 +19,5 @@ public class PartyTest {
 
 //    @Test
 //    public void
->>>>>>> a0e26e25fdb9f768674714db48dee76b0e23404d
+
 }
